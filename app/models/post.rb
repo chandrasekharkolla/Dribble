5ff110fb518410ad5eadd31153eac802e6f8b127
chandrasekharkolla <chandrasekharkolla@gmail.com>
2016-03-08 16:00:08 -0500

    Add styling

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-	has_attached_file :image, styles: { medium: "750x550#", small: "100x100#" }
+	has_attached_file :image, styles: { medium: "750x550", small: "350x250#" }
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   	belongs_to :user
   	has_many :comments
